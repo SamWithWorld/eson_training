b651b4bf19ec481a9efd7fd914d83b1262158e55
lovingJune <myjobcareer@163.com>
2014-12-27 23:49:21 +0800

    login03

--- login_day03.py
+++ login_day03.py
@@ -1,5 +1,7 @@
 #coding:utf-8
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 import unittest
 import time
 '''
@@ -13,21 +15,27 @@ class CreatPost(unittest.TestCase):
         self.username = 'admin'
         self.passwd   = 'admin'
 
-##    def testLogin(self):
-##
-##        #登陆操作
-##        self.login(self.username,self.passwd)
-##
-##        self.assertIn('wp-admin',self.driver.current_url)
-##        # 检查用户信息 欢迎你，admin
-##        self.assertTrue(self.username in self.driver.find_element_by_css_selector('li#wp-admin-bar-my-account a').text)
+#暂时注释掉了登陆用例
+    def testLogin(self):
+        print 'testLogin'
 
-    def testCreatePost(self):
-        print 'CreatePost'
+        #登陆操作
         self.login(self.username,self.passwd)
 
+        self.assertIn('wp-admin',self.driver.current_url)
+        # 检查用户信息 欢迎你，admin
+        self.assertTrue(self.username in self.driver.find_element_by_css_selector('li#wp-admin-bar-my-account a').text)
+
+    def testCreatePost(self):
+        print 'testCreatePost'
+        self.login(self.username,self.passwd)
+
+        # title 由当前时间组成，以免输入固定字符Sam、Test Content
+        title = 'Sam'+time.strftime("%Y-%m-%d %H:%M:%S")
+
         #创建 新文章
-        self.createPost() # title = self.createPost()
+        #self.createPost()
+        self.createPost(title)
 
         #进入首页，进行断言
         self.driver.get('http://localhost/wordpress/')
@@ -35,16 +43,35 @@ class CreatPost(unittest.TestCase):
         self.assertEqual(title,firstpost.text)
 
     def testDeletePost(self):
+        print 'testDeletePost'
         '''
         1.先登陆，创建一篇文章，这样可以保证测试用例的健壮，删除时有数据可删
         '''
         #登陆
         self.login(self.username,self.passwd)
-        self.createPost()
+        #创建一篇文章，并获取文章id
+        post_id = self.createPost()
 
         #进入 所有文章 页面，准备删除文章
         self.driver.get("http://localhost/wordpress/wp-admin/edit.php")
-        # xxxxxxxxxxxxxx
+        #拼接文章id
+        rowId = 'post-'+post_id
+        # 定位新创建的文章，准备删除
+        row = self.driver.find_element_by_id(rowId)
+        ActionChains(self.driver).move_to_element(row).perform()
+        #定位 移至回收站 元素
+        trashlink = row.find_element_by_css_selector('span.trash a')
+        trashlink.click()
+        self.assertFalse(self.isPostRowExists(rowId))
+
+    def isPostRowExists(self,rowId):
+        try:
+            self.driver.find_element_by_id(rowId)
+            return True
+
+        except NoSuchElementException as e:
+            return False
+
 
     def tearDown(self):
         print 'End test'
@@ -65,13 +92,13 @@ class CreatPost(unittest.TestCase):
         js = "document.getElementById('content_ifr').contentWindow.document.body.innerHTML='"+content+"'"
         self.driver.execute_script(js)
 
-    def createPost(self):
-        # title 由当前时间组成，以免输入固定字符Sam、Test Content
-        title = 'Sam'+time.strftime("%Y-%m-%d %H:%M:%S")
+    def createPost(self,title='Default Title'):
+        # content 由当前时间组成，以免输入固定字符Test Content
+        #title = 'Sam'+time.strftime("%Y-%m-%d %H:%M:%S")
         content = 'Test Content'+time.strftime("%Y-%m-%d %H:%M:%S")
 
         #调用添加文章的方法
-        self.createArticle(title,content)
+        return self.createArticle(title,content)
         #return title
 
     def createArticle(self,title,content):
@@ -90,16 +117,10 @@ class CreatPost(unittest.TestCase):
         return self.fetchPostId()
 
     def fetchPostId(self):
-        postUrl = self.driver.find_element_by_id("sample-permalink")
+        postUrl = self.driver.find_element_by_id("sample-permalink").text
 
         #获得 文章的链接地址，如 http://localhost/wordpress/?p=25，并获得文章id号
-        return postUrl.split("=")[1]
-
-
-
-
-
-
+        return postUrl.split("=")[-1]
 
 if __name__ == '__main__':
     unittest.main()
